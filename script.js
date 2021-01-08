b257c1fffaae2ed1b9bb3669dd803cd7b14b7c34
Auddity <audsecondarylap@outlook.com>
2021-01-08 08:23:30 -0500

    active add

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const toggle = document.getElementById('slider');
 
 toggle.addEventListener('click', () => document.body.classList.toggle('dark'));
+
+// TODO: Add hover states & added javascript features
